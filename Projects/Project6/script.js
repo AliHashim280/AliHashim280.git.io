e35cbabf0c9aaac711befd5334e8d8e492945024
Ali-Hashim-tech <161108948+AliHashim280@users.noreply.github.com>
2024-09-06 21:08:29 +0500

    0.5

--- Projects/Project6/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function () {
-  // Toggle sidebar visibility
-  document.querySelector('.sidebar').classList.toggle('active');
-  
-  // Animate the hamburger icon (optional)
-  this.classList.toggle('open');
-});
